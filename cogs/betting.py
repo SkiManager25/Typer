0f5d57e3649d5923f2b9f4b115505e19885dd87f
SkiManager25 <skijumpingrp@gmail.com>
2026-08-24 15:28:49 +0200

    Update betting.py

--- cogs/betting.py
+++ cogs/betting.py
@@ -299,7 +299,7 @@ class Betting(commands.Cog):
             await interaction.response.send_message("📋 Brak otwartych meczów w tej chwili.")
             return
 
-        LIMIT = 24
+        LIMIT = 48
         shown = matches[:LIMIT]
 
         embed = discord.Embed(
